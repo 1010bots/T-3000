41bfa0e43dcfd80987891438fa697a6672a846db
Reinhart Previano Koentjoro <reinhart@reinhart1010.id>
2023-07-31 00:32:18 +0700

    feat: (>_ ): introduce new favicon

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,4 +1,4 @@
-<picture>
+{{-- <picture>
   <source srcset="/img/icons/shell-blue-male-dark.avif" media="(prefers-color-scheme:dark)" type="image/avif" />
   <source srcset="/img/icons/shell-blue-male-dark.heic" media="(prefers-color-scheme:dark)" type="image/heic" />
   <source srcset="/img/icons/shell-blue-male-dark.webp" media="(prefers-color-scheme:dark)" type="image/webp" />
@@ -8,4 +8,5 @@
   <source srcset="/img/icons/shell-blue-male-light.webp" media="(prefers-color-scheme:light)" type="image/webp" />
   <source srcset="/img/icons/shell-blue-male-light.png" media="(prefers-color-scheme:light)" type="image/png" />
   <img alt="Reinhart Previano K." src="/img/icons/shell-blue-male-neutral.png" height="668" width="691" class="h-12 w-auto" />
-</picture>
+</picture> --}}
+<img alt="Reinhart Previano K." src="/favicon.ico" height="16" width="16" class="h-12 w-12 m-auto" style="image-rendering: pixelated;" />
