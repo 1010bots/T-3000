f6afb7cb0166723b17a4e5196386e958433376a1
Reinhart Previano Koentjoro <reinhart@reinhart1010.id>
2023-10-14 01:37:20 +0700

    fix: (>_ ): proper 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,3 +1,6 @@
 <x-app-layout>
-    
+    <div class="p-4 text-center text-black dark:text-white">
+        <h1 class="font-serif text-8xl font-light">404</h1>
+        <p class="font-serif text-2xl">Not Found</p>
+    </div>
 </x-app-layout>
