cb2abfcb2372a4e7af1b75b4eafeed4122c331d5
Reinhart Previano Koentjoro <reinhart@reinhart1010.id>
2024-01-20 15:58:56 +0700

    fix: make mobile hamburger adaptive

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -153,7 +153,7 @@
 
                 <!-- Hamburger -->
                 <div class="flex items-center sm:hidden">
-                    <x-primitives.card element="button" aria-label="Site Menu" @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md">
+                    <x-primitives.card :theme-color="$theme_color" element="button" aria-label="Site Menu" @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md">
                         <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                             <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                             <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
