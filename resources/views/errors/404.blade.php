6b9238b3e8bba99d21747e63147fe131520664ec
Reinhart Previano Koentjoro <reinhart@reinhart1010.id>
2024-06-19 23:07:09 +0700

    fix: 404 page for mobile

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,16 +1,15 @@
 <x-app-layout theme-scheme="dark">
     <div class="flex flex-col gap-4 items-center justify-center p-4 pt-24 text-black dark:text-white">
         <div class="flex items-start">
-            <p style="font-size: 16rem; line-height:100%; margin-inline-end: -3rem;">4</p>
+            <p style="font-size: min(max(calc(3rem + 20vw), 8rem), 16rem); line-height:100%; margin-inline-end: -3rem;">4</p>
             <picture>
                 <source src="/img/full-body/shiftine-holo.avif" type="image/avif">
                 <source src="/img/full-body/shiftine-holo.webp" type="image/webp">
                 <source src="/img/full-body/shiftine-holo.png" type="image/png">
-                <img src="/img/full-body/shiftine-holo.png" class="h-80 w-80">
+                <img src="/img/full-body/shiftine-holo.png" class="h-40 sm:h-60 lg:h-80 w-40 sm:w-60 lg:w-80">
             </picture>
-            <p style="font-size: 16rem; line-height:100%; margin-inline-start: -6rem;">4</p>
-
+            <p style="font-size: min(max(calc(3rem + 20vw), 8rem), 16rem); line-height:100%; margin-inline-start: max(min(calc(4rem - 20vw), -3rem), -6rem);">4</p>
         </div>
-        <p class="text-xl text-center">&ldquo;When someone is lost in the cyberspace, the Site Chief has to get them back.<br />Anyway, nice to meet you! I&rsquo;m renovating my home so you can pay a visit to me (&gt;_ )!&rdquo;</p>
+        <p class="sm:text-xl text-center" style="max-width: 49rem">&ldquo;When someone is lost in the cyberspace, the Site Chief has to get them back. Anyway, nice to meet you! I&rsquo;m renovating my home so you can pay a visit to me (&gt;_ )!&rdquo;</p>
     </div>
 </x-app-layout>
